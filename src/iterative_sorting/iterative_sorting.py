0b31d33b5d74247dfa890d5832de0b1f9746b9b0
Kevin Tou <toukevin@gmail.com>
2019-12-09 12:00:50 -0800

    Added comments to break down logic flow

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,11 +7,15 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         for j in range(cur_index, len(arr) - 1):
+            # Update the smallest_index if the current value is smaller
             if arr[j + 1] < arr[smallest_index]:
                 smallest_index = j + 1
             # TO-DO: swap
+        # Temporarily hold the value of the array[cur_index] before overwriting it
         temp = arr[cur_index]
+        # Sets the cur_index's value to the smallest_index's value
         arr[cur_index] = arr[smallest_index]
+        # Complete the final swap by assigning the smallest_index's value to the cur_index's value
         arr[smallest_index] = temp
     return arr
 
