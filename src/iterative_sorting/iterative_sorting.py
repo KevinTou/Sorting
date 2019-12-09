7b9dbbd8d9470ef82c09936a374dcf15dfcdd2ad
Kevin Tou <toukevin@gmail.com>
2019-12-09 12:34:13 -0800

    Finished with bubble sort w/ comments

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -23,7 +23,23 @@ def selection_sort(arr):
 
 
 def bubble_sort(arr):
+    # The sort will keep running until no more swaps are made
+    while True:
+        # The initialization of the ending condition
+        swap = 0
 
+        # Goes through the array and swaps if the value on the RHS is smaller than the current value
+        for i in range(0, len(arr) - 1):
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                # Increases swap count
+                swap += 1
+
+        # Breaks when the for loop can make a clean pass
+        if swap == 0:
+            break
     return arr
 
 
